dbf9a2dff5662b8dec7ffe2abd27a653f0c02831
fpetros <fernandopetros@gmail.com>
2023-03-22 22:22:44 +0000

    - Adjust notifications padding in waybar - Create voice file in .temp folder in voice-recorder-drag-drop.sh - Don't open notification menu with click on waybar

--- scripts/voice-recorder-drag-drop.sh
+++ scripts/voice-recorder-drag-drop.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-VOICE_RECORDER_FILE="$HOME/voicemessage.ogg"
+TEMP_FOLDER="$HOME/.temp"
+VOICE_RECORDER_FILE="$TEMP_FOLDER/voicemessage.ogg"
 RECORD_COMMAND="ffmpeg -f pulse -i default -ac 1 -ar 48000 -c:a libopus -y $VOICE_RECORDER_FILE"
+
+mkdir -p $TEMP_FOLDER
 
 alacritty --config-file "$ALACRITTY_CONFIG" --class "voice-recorder-drag-drop" -e $RECORD_COMMAND 
 
-dragon-drop $VOICE_RECORDER_FILE
+dragon-drop -T $VOICE_RECORDER_FILE
 
 rm $VOICE_RECORDER_FILE
 
