439edf43db7cf2699d6ecb4990bbe0142d5363ed
fpetros <fernandopetros@gmail.com>
2023-03-31 20:58:09 +0100

    - Added Blank picture to preload wallpaper, set wallpaper from   $WALLPAPER in the scripts - Added swayidle to packages - Automatically get Kernel name in arch-updater - Updated powermenu script to use $WALLPAPER - Changed startup priority - Added idle script to lock/turn off monitors

--- scripts/arch-updater.sh
+++ scripts/arch-updater.sh
@@ -2,7 +2,7 @@
 
 paru -Syu
 
-KERNEL="linux-cachyos-bmq"
+KERNEL=$(paru -Q | grep "^linux" | cut -d' ' -f1 | grep -v firmware | grep -v api | grep -v headers)
 
 sudo mkinitcpio -p $KERNEL
 sudo grub-mkconfig -o /boot/grub/grub.cfg
--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -4,8 +4,6 @@ entries=" Lock\n⇠   Logout\n⏾ Suspend\n󰑓 Reboot\n⏻ Shutdown"
 
 selected=$(echo -e $entries|wofi -i --width 220 --height 200 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
-WALLPAPER=~/Nextcloud/Wallpapers/cat_pacman.png
-
 case $selected in
   lock)
 	  swaylock -i "$WALLPAPER";;
