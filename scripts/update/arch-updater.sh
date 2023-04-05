bbf49f31222cb6f85ca609671d89ef345164f916
fpetros <fernandopetros@gmail.com>
2023-04-05 13:07:26 +0100

    - script-hub now waits for input before closing - Commented Kernel section of arch-updater, did not solve the problem it   was supposed to - os-updater can now be run in the terminal or gui - Updated pathing of startup script in hyprland.conf

--- scripts/update/arch-updater.sh
+++ scripts/update/arch-updater.sh
@@ -2,8 +2,7 @@
 
 paru -Syu
 
-KERNEL=$(paru -Q | grep "^linux" | cut -d' ' -f1 | grep -v firmware | grep -v api | grep -v headers)
-
-sudo mkinitcpio -p $KERNEL
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+#KERNEL=$(paru -Q | grep "^linux" | cut -d' ' -f1 | grep -v firmware | grep -v api | grep -v headers)
+#sudo mkinitcpio -p $KERNEL
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
 
--- scripts/update/os-updater.sh
+++ scripts/update/os-updater.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-perform_update() {
-	CMD=$([[ $TERMINAL == "alacritty" ]] && echo "-e $SHELL -c $UPDATE_COMMAND" || echo "$SHELL -c $UPDATE_COMMAND")
-	$TERMINAL --class update --hold $CMD
-}
+GUI="$1"
+
+if [ "$GUI" == "gui" ]; then
+	CMD=$([[ $TERMINAL == "alacritty" ]] && echo "-e $0" || echo "$0")
+	exec $TERMINAL --class "update" --hold $CMD
+	exit
+fi
 
 OS_UPDATE_COMMAND="arch-updater"
 FLATPAK_UPDATE_COMMAND="flatpak update"
 
 FLATPAK_COMMAND=$(command -v flatpak)
 
-if [ ! -z $FLATPAK_COMMAND ]; then	
-	UPDATE_COMMAND="$OS_UPDATE_COMMAND && $FLATPAK_UPDATE_COMMAND"
-	perform_update
-	exit
-fi
-
 UPDATE_COMMAND="$OS_UPDATE_COMMAND"
-perform_update
+
+echo -e "\n────────── System Update ──────────\n"
+$SHELL -c "$UPDATE_COMMAND"
+
+if [ ! -z $FLATPAK_COMMAND ]; then
+	echo -e "\n────────── Flatpak Update ──────────\n"
+	$SHELL -c "$FLATPAK_UPDATE_COMMAND"
+fi
 
